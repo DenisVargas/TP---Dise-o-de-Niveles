733c6c34d0095fdff52a0417d4982a9051757efd
DenisVargas <38993740+DenisVargas@users.noreply.github.com>
2018-06-23 16:14:15 -0300

    Projecto de Muestra
    Projecto Inicial

---
+++ Assets/Scripts/PlayerMorido.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMorido : MonoBehaviour {
+    public GameObject PuntoInicial;
+    public Vector3 PosicionRespawn;
+
+
+	// Use this for initialization
+	void Start () {
+        PosicionRespawn = PuntoInicial.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.y < -21)
+        {
+            transform.position = PosicionRespawn;
+        }
+	}
+}
